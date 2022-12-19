d70bca6f3de15eeb3a13d6e68c8a5c24fe3cff91
LeeJE20 <silver36c@gmail.com>
2022-12-19 13:01:39 +0900

    FEAT: dotenv 설정

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ var express = require("express");
 var path = require("path");
 // var cookieParser = require("cookie-parser");
 // var logger = require("morgan");
-
-var indexRouter = require("./src/routes/index");
-var usersRouter = require("./src/routes/users");
+const dotenv = require("dotenv");
+dotenv.config();
+const indexRouter = require("./src/routes/index");
 
 var app = express();
 app.use(require("connect-history-api-fallback")());
